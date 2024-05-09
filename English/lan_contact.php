a57bb128a0b06d9cf2c2370e8c497b079a57126e
leo <the_guardian2@hotmail.com>
2024-05-09 19:18:29 +0200

    Add files via upload

---
+++ English/lan_contact.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * e107 website system
+ *
+ * Copyright (C) 2008-2016 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ * Contact language file
+ *
+ * $Source: /cvs_backup/e107_0.8/e107_languages/English/lan_contact.php,v $
+ * $Revision$
+ * $Date$
+ * $Author$
+ */
+
+
+define("LAN_CONTACT_00", "Contact Us");
+define("LAN_CONTACT_01", "Contact Details");
+define("LAN_CONTACT_02", "Contact Form");
+define("LAN_CONTACT_03", "Enter your name");
+define("LAN_CONTACT_04", "Enter your e-mail address");
+define("LAN_CONTACT_05", "Message subject");
+define("LAN_CONTACT_06", "Message");
+define("LAN_CONTACT_07", "Email a copy of this message to your own address ");
+define("LAN_CONTACT_08", "Send");
+define("LAN_CONTACT_09", "Your message was sent.");
+define("LAN_CONTACT_10", "There was a problem sending your message.");
+define("LAN_CONTACT_11", "Please check your email address and resubmit the contact form.");
+define("LAN_CONTACT_12", "Your message is too short.");
+define("LAN_CONTACT_13", "Please include a subject.");
+
+define("LAN_CONTACT_14", "Send message to");
+define("LAN_CONTACT_15", "Incorrect code entered");
+define("LAN_CONTACT_16", "You must be [registered] and signed-in to use this form.");
+define("LAN_CONTACT_17", "Please enter your name.");
+define("LAN_CONTACT_18", "Please enter your e-mail address.");
+define("LAN_CONTACT_19", "Please enter the subject for your e-mail.");
+define("LAN_CONTACT_20", "Please enter your message for us.");
+define("LAN_CONTACT_21", "I consent to having this website store my submitted information so they can respond to my inquiry"); //   By using this form, you agree to the storage and processing of your data through this site.");
+define("LAN_CONTACT_22", "Privacy policy");
+define("LAN_CONTACT_23", "You can find our privacy policy here: [x]");
+define("LAN_CONTACT_24", "GDPR Agreement");
+
+
